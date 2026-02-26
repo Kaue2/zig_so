cd70f697ca7905cf8ac31dccdd0c899ccf797b00
Kauê Dos Anjos <kaueo.anjos@gmail.com>
2026-02-26 18:16:52 -0300

    add: implementar primeira versão do kernel

--- kernel.zig
+++ kernel.zig
@@ -6,18 +6,36 @@ extern var __bss: [0]u8;
 extern var __bss_end: [0]u8;
 extern var __stack_top: [0]u8;
 
-fn memset(buf: *anyopaque, c: u8, n: size_t) *anyopaque {
+fn memset(buf: *anyopaque, c: c_char, n: usize) *anyopaque {
     var p: [*]u8 = @ptrCast(buf);
+    const value: u8 = @intCast(c);
     var i: size_t = 0;
+
     while (i < n) : (i += 1) {
-        p[i] = c;
+        p[i] = value;
     }
+
     return buf;
+}
+
+export fn kernel_main() void {
+    _ = memset(&__bss, 0, @intFromPtr(&__bss_end) - @intFromPtr(&__bss));
+
+    while (true) {}
+}
+
+export fn boot() linksection(".text.boot") callconv(.naked) void {
+    asm volatile (
+        \\mv sp, %[stack_top]
+        \\j kernel_main
+        :
+        : [stack_top] "r" (&__stack_top),
+    );
 }
 
 test "memset fills a buffer" {
     // setup buffer
-    var buffer: [5]u8 = [_]u8{ 0, 0, 0, 0, 0 };
+    var buffer: [5]u8 = [_]u8{ 'A', 'A', 'A', 'A', 'A' };
 
     _ = memset(&buffer, 0, 3);
 
@@ -25,5 +43,5 @@ test "memset fills a buffer" {
     try std.testing.expectEqual(buffer[1], 0);
     try std.testing.expectEqual(buffer[2], 0);
 
-    try std.testing.expectEqualSlices(u8, &[_]u8{ 'A', 'A', 'A', 0, 0 }, &buffer);
+    try std.testing.expectEqualSlices(u8, &[_]u8{ 0, 0, 0, 'A', 'A' }, &buffer);
 }
