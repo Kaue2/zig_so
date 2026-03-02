5100e08a8b86f9c38e2fbb28360adc91096bf614
Kauê Dos Anjos <kaueo.anjos@gmail.com>
2026-03-02 15:30:31 -0300

    refac: alterar chamada de dependências

--- kernel.zig
+++ kernel.zig
@@ -1,33 +1,9 @@
+const commom = @import("common.zig");
 const size_t = u32;
 
 extern var __bss: [0]u8;
 extern var __bss_end: [0]u8;
 extern var __stack_top: [0]u8;
-
-const Sbiret = struct {
-    error_code: isize, // long do c
-    value: isize,
-};
-
-fn sbi_call(arg0: i32, arg1: i32, arg2: i32, arg3: i32, arg4: i32, arg5: i32, fid: i32, eid: i32) Sbiret {
-    var err: isize = undefined;
-    var val: isize = undefined;
-
-    asm volatile ("ecall"
-        : [err] "={a0}" (err),
-          [val] "={a1}" (val),
-        : [arg0] "{a0}" (arg0),
-          [arg1] "{a1}" (arg1),
-          [arg2] "{a2}" (arg2),
-          [arg3] "{a3}" (arg3),
-          [arg4] "{a4}" (arg4),
-          [arg5] "{a5}" (arg5),
-          [fid] "{a6}" (fid),
-          [eid] "{a7}" (eid),
-        : .{ .memory = true });
-
-    return Sbiret{ .error_code = err, .value = val };
-}
 
 fn memset(buf: *anyopaque, c: c_char, n: usize) *anyopaque {
     var p: [*]u8 = @ptrCast(buf);
@@ -41,17 +17,9 @@ fn memset(buf: *anyopaque, c: c_char, n: usize) *anyopaque {
     return buf;
 }
 
-fn putchar(ch: u8) void {
-    _ = sbi_call(ch, 0, 0, 0, 0, 0, 0, 1);
-}
-
 export fn kernel_main() void {
-    const s: [*]const u8 = "\n\n Hello world!\n";
-
-    var i: usize = 0;
-    while (s[i] != 0) : (i += 1) {
-        putchar(s[i]);
-    }
+    commom.printf("\n\n Hello %s\n", .{"World"});
+    commom.printf("\n\n 1 + 2 = %d \n\n", .{1 + 2});
 
     while (true) {
         asm volatile ("wfi");
